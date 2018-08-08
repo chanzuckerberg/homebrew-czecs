9727949c719dbb0d3a6a709c700bb60651020add
goreleaserbot <goreleaser@carlosbecker.com>
2018-08-08 09:28:43 -0700

    Brew formula update for czecs version v0.0.1

--- czecs.rb
+++ czecs.rb
@@ -1,9 +1,9 @@
 class Czecs < Formula
   desc "ECS release management tool."
   homepage "https://github.com/chanzuckerberg/czecs"
-  url "https://github.com/chanzuckerberg/czecs/releases/download/0.1.0/czecs_0.1.0_darwin_amd64.tar.gz"
-  version "0.1.0"
-  sha256 "fa2fcea2eebde565f7f7d622539f31efe48cb84e05ade88252057af7ad541b4e"
+  url "https://github.com/chanzuckerberg/czecs/releases/download/v0.0.1/czecs_0.0.1_darwin_amd64.tar.gz"
+  version "0.0.1"
+  sha256 "296731aecf3028405e5c16052df36ed5e053c612ffcc1011b06a8c5f5ff8adb7"
 
   def install
     bin.install "czecs"
